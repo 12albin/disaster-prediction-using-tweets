ad6a2c8600e5ebc74ae1a44e133352df013719ab
Albin T S <albints8111@gmail.com>
2024-07-27 11:55:05 +0530

    updated code v1

---
+++ app.py
@@ -0,0 +1,25 @@
+import pickle
+import streamlit as stl
+import time
+from sklearn.feature_extraction.text import TfidfVectorizer as tf
+
+stl.title("Disaster tweet identification")
+model=pickle.load(open('model.pkl','rb'))
+
+tweet=stl.text_input("enter the tweet")
+
+
+submit=stl.button("predict")
+
+if submit:
+    #start_time=time.time()
+    pred=model.predict([tweet])
+    #end_time=time.time()
+    #stl.write("prediction time taken:",round(end_time-start_time,2),'seconds')
+    if pred[0]==0:
+        stl.write("Prediction: No warning nagative!")
+    else:
+        stl.write("Prediction: Disaster warning, Positive!")
+
+
+
